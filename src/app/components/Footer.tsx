9e58ba2fd7b9a46ae7f18ccc49737f1eaa971b77
sarahguterres <sarah.12dng@gmail.com>
2025-04-24 17:37:00 -0300

    Página Footer Alterada

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
       <footer className="bg-gray-800 text-white py-4">
         <div className="container mx-auto text-center">
           <p className="text-sm">
-            © {new Date().getFullYear()} Meu Portfólio. Todos os direitos reservados.
+            © {new Date().getFullYear()} T.I. Todos os direitos reservados.
           </p>
           <p className="text-xs mt-1">Desenvolvido com Next.js e Tailwind CSS</p>
           <p className="text-xs mt-1">
